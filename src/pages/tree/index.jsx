39bd9d112c427b5cda3a04b3ebb7b07371df9b53
JustSaul <ksauliuskk@gmail.com>
2019-11-12 20:12:47 +0200

    chore/move-tree-page-to-component

--- src/pages/tree/index.jsx
+++ src/pages/tree/index.jsx
@@ -1,13 +1,1 @@
-import React from 'react'
-import Provider from '../../components/tree/provider'
-
-import { TYPE as TREE_TYPE } from '../../utils/tree'
-
-const TreePage = (props) => (
-    <>
-        <Provider {...props} type={TREE_TYPE.RECURSIVE}/>
-        <Provider {...props} type={TREE_TYPE.ITERATIVE}/>
-    </>
-)
-
-export default TreePage
+export { default } from './component'
